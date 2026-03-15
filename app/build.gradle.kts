f01d96587838b64c9cbefc53866cec75adcf8805
Abdo <manabdo54321@gmail.com>
2026-03-16 01:01:31 +0200

    Fix: Add icons dependency to app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.icons)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.hilt.android)
     implementation(libs.hilt.navigation.compose)
